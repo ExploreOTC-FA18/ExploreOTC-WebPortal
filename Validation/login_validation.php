3f61cbadf3c6defaca68863adcd508661b16100a
Nicholas Ingram <ingram.nicholas.a@gmail.com>
2018-11-07 21:00:16 -0600

    Added login validation to the server

---
+++ Validation/login_validation.php
@@ -0,0 +1,14 @@
+<?php
+    //// TODO: This must go at the top of all the webpages for the portal
+    //      so a user can't just enter the url and access the information
+    //Also the username and password will be different on the actual website
+    include('../DB/conn.php');
+    session_start();
+
+    if (validate_password(strtolower($_POST["username"]), $_POST["password"])) {
+        $_SESSION["username"] = $_POST["username"];
+        header("Location: ../validation_test.php");
+    } else {
+        header("Location: ../index.php?login=false");
+    }
+ ?>
