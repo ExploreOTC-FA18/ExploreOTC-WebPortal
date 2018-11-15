343ec2f3debf830df952c012f438bf2609bb4e1a
Unknown <sw0113896@otc.edu>
2018-11-15 05:15:17 -0600

    Dashboard
    Created the main dashboard

---
+++ dashboard.php
@@ -0,0 +1,132 @@
+<!-- https://learn.getgrav.org/user/pages/05.admin-panel/09.faq/faq_2.png -->
+<!-- http://ionicons.com/ -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>ExploreOTC Dashboard</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="vendors/css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="vendors/css/grid.css">
+    <link rel="stylesheet" type="text/css" href="vendors/css/ionicons.min.css">
+    <link rel="stylesheet" href="Styles/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,300i,400" rel="stylesheet">
+</head>
+<body>
+    <div style="width: 100vw">
+        <div style="height: 100vh">
+            <section class="side-nav">
+                <div class="title-button">
+                    ExploreOTC 
+                </div>
+                <div class="menu-functions">
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">
+                                <i class="ion-speedometer">&nbsp;&nbsp</i>
+                                Dashboard
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <i class="ion-clipboard">&nbsp;&nbsp</i>
+                                Registration
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <i class="ion-filing">&nbsp;&nbsp</i>
+                                QR Code 
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <i class="ion-person-stalker">&nbsp;&nbsp</i>
+                                Survey
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+            <nav class="main-nav navbar navbar-expand-sm">
+                <div class="container">
+                    <a class="navbar-brand" href="#"></a>
+                    <div>
+                        <a href="#"><i class="ion-gear-b"></i></a>
+                        &nbsp;&nbsp;&nbsp;&nbsp;
+                        <a href="#"><i class="ion-log-out"></i></a>
+                    </div>
+                </div>
+            </nav>
+            <section class="main-content">
+                <div class="content-title-bar">
+                    <h4 class="content-title">Dashboard</h4>
+                </div>
+                <div class="content-path">
+                    <i class="ion-speedometer">&nbsp;&nbsp</i>
+                    <p class="path">Home > Dashboard</p>
+                </div>
+                <div class="card-deck mt-4">
+                    <div class="card card-blue">
+                        <div class="card-body">
+                            <div class="card-body-body">
+                                <br><h4 class="card-title">xx</h4>
+                                <p class="card-text">Number of Students Registered</p>
+                            </div>
+                            <div class="card-body-icon">
+                                <i class="ion-filing"></i>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="card-footer">
+                                <p></p>
+                                <i class="ion-arrow-right-b"></i>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="card card-orange">
+                        <div class="card-body">
+                            <div class="card-body-body">
+                                <br><h4 class="card-title">xx</h4>
+                                <p class="card-text">Number of QR Scans</p>
+                            </div>
+                            <div class="card-body-icon">
+                                <i class="ion-email"></i>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="card-footer">
+                                <p></p>
+                                <i class="ion-arrow-right-b"></i>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="card card-red">
+                        <div class="card-body">
+                            <div class="card-body-body">
+								<br><h4 class="card-title">xx</h4>
+                                <p class="card-text">Number of Surveys Completed</p>
+                            </div>
+                            <div class="card-body-icon">
+                                <i class="ion-ios-calendar"></i>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="card-footer">
+                                <p></p>
+                                <i class="ion-arrow-right-b"></i>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
